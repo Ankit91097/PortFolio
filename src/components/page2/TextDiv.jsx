d6e9a8f220c19a42668c85c2141281c02a10c2a3
Ankit Niranjan <ankitniranjan331@gamil.com>
2024-07-16 23:13:04 +0530

    page2 animation

--- src/components/page2/TextDiv.jsx
+++ src/components/page2/TextDiv.jsx
@@ -6,21 +6,24 @@ const TextDiv = () => {
     const hoverRef = useRef(null)
 
     const mouseEntering=()=>{
-
+      hoverRef.current.style.transform='translate(-50%,-50%) scale(1)'
     }
     const mouseLeaving=()=>{
-        
+        hoverRef.current.style.transform='translate(-50%,-50%) scale(0)'
     }
-    const mouseMoving=()=>{
-        
+    const mouseMoving=(e)=>{
+        const realXposition=e.clientX-overlayRef.current.getBoundingClientRect().x
+        const realYposition=e.clientY-overlayRef.current.getBoundingClientRect().y
+        hoverRef.current.style.left=realXposition+"px"
+        hoverRef.current.style.top=realYposition+"px"
     }
   return (
-    <div id='parent' ref={overlayRef}
+    <div id='parent' className='relative overflow-hidden' ref={overlayRef}
      onMouseEnter={mouseEntering}
      onMouseLeave={mouseLeaving}
      onMouseMove={(e)=>{mouseMoving(e)}}
       >
-        <div ref={hoverRef} className='absolute h-[16vw] w-[17vw] bg-red-600 overflow-hidden'>
+        <div ref={hoverRef} className='image absolute overflow-hidden top-0 -translate-x-1/2 -translate-y-1/2 h-[16vw] w-[17vw] bg-red-600 '>
         <motion.div className='h-full w-full'>
             <img className='h-full w-full object-cover' src="https://dennissnellenberg.com/media/pages/work/twice/0ab7e43954-1710404752/thumbnail-twice-810x810-crop-q72.jpg" alt="" />
             <img className='h-full w-full object-cover'  src="https://dennissnellenberg.com/media/pages/work/the-damai/b511d32d21-1710452224/thumbnail-thedamai-v2-810x810-crop-q72.jpg" alt="" />
@@ -30,22 +33,29 @@ const TextDiv = () => {
         </div>
         
        <div>
-       <div className='hover:px-[7vw] hover:text-gray-600 transition-all border-t-2 flex items-center justify-between px-[2vw] py-[2vw]'>
-            <h1 className='text-8xl uppercase'>Twice</h1>
-            <h5 className='text-xl opacity-85'>Ineteraction & Development</h5>
+        <div className='relative'>
+       <div className='hover:px-[7vw] relative hover:text-gray-600 transition-all border-t-2 flex items-center justify-between px-[2vw] py-[2vw]'>
+            <div className='overlay h-full w-full absolute z-50 top-0 left-0'></div>
+            <h1 className='text-8xl uppercase z-10'>Twice</h1>
+            <h5 className='text-xl opacity-85 z-10'>Ineteraction & Development</h5>
         </div>
-        <div className='hover:px-[7vw] hover:text-gray-600 transition-all border-t-2 flex items-center justify-between px-[2vw] py-[2vw]'>
-            <h1 className='text-8xl uppercase'>Twice</h1>
-            <h5 className='text-xl opacity-85'>Ineteraction & Development</h5>
+        <div className='hover:px-[7vw] relative hover:text-gray-600 transition-all border-t-2 flex items-center justify-between px-[2vw] py-[2vw]'>
+            <div className='overlay h-full w-full absolute z-50 top-0 left-0'></div>
+            <h1 className='text-8xl uppercase z-10'>Twice</h1>
+            <h5 className='text-xl opacity-85 z-10'>Ineteraction & Development</h5>
         </div>
-        <div className='hover:px-[7vw] hover:text-gray-600 transition-all border-t-2 flex items-center justify-between px-[2vw] py-[2vw]'>
-            <h1 className='text-8xl uppercase'>Twice</h1>
-            <h5 className='text-xl opacity-85'>Ineteraction & Development</h5>
+        <div className='hover:px-[7vw] relative hover:text-gray-600 transition-all border-t-2 flex items-center justify-between px-[2vw] py-[2vw]'>
+            <div className='overlay h-full w-full absolute z-50 top-0 left-0'></div>
+            <h1 className='text-8xl uppercase z-10'>Twice</h1>
+            <h5 className='text-xl opacity-85 z-10'>Ineteraction & Development</h5>
         </div>
-        <div className='hover:px-[7vw] hover:text-gray-600 transition-all border-t-2 flex items-center justify-between px-[2vw] py-[2vw] border-b-2'>
-            <h1 className='text-8xl uppercase'>Twice</h1>
-            <h5 className='text-xl opacity-85'>Ineteraction & Development</h5>
+        <div className='hover:px-[7vw] relative hover:text-gray-600 transition-all border-t-2 flex items-center justify-between px-[2vw] py-[2vw]'>
+            <div className='overlay h-full w-full absolute z-50 top-0 left-0'></div>
+            <h1 className='text-8xl uppercase z-10'>Twice</h1>
+            <h5 className='text-xl opacity-85 z-10'>Ineteraction & Development</h5>
         </div>
+        </div>
+        
        </div>
     </div>
   )
